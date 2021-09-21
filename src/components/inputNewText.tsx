f779900102bcd604f2da3b1d9717c884d5ead89b
StanHus <guseletovS@gmail.com>
2021-09-21 15:07:59 +0100

    app works

---
+++ src/components/inputNewText.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function InputText (): JSX.Element {
+  const [text, setText] = useState("");
+
+  const onSubmitForm = async (e: any) => {
+    e.preventDefault();
+    try {
+      const body = { text };
+      const response = await fetch("http://cryptic-plains-98206.herokuapp.com/list", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+
+      window.location.href = "/";
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <main>
+      <h1>Paste into here</h1>
+      <form onSubmit={onSubmitForm}>
+        <input
+          type="text"
+          placeholder="Put the text here"
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
+    </main>
+  );
+};
+
+export default InputText;
---
+++ src/components/listInputs.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import "../css/style.css"
+
+interface Iinput {
+    id: number,
+    text: string
+}
+const ListInputs = () => {
+  const [inputs, setInputs] = useState([]);
+
+  const getinputs = async () => {
+    try {
+      const response = await fetch("http://cryptic-plains-98206.herokuapp.com/list");
+      const jsonData = await response.json();
+      setInputs(jsonData);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    getinputs();
+  }, []);
+
+  console.log(inputs);
+
+  return (
+    <main className = "table">
+      {" "}
+      <table>
+        <tbody>
+          {inputs.map((input: Iinput) => (
+            <tr key={input.id}>
+              <td>{input.text}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </main>
+  );
+};
+
+export default ListInputs;
